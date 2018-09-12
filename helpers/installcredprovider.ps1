cbbc460fde6c84bd8d19aece4005e69aeff047c3
Satu Bailey <satbai@microsoft.com>
2018-09-12 16:02:20 -0700

    Add option to check if the plugin already exists

--- helpers/installcredprovider.ps1
+++ helpers/installcredprovider.ps1
@@ -2,10 +2,12 @@
 # plugin for Dotnet and/or NuGet to ~/.nuget/plugins directory
 # To install netcore, run installcredprovider.ps1
 # To install netcore and netfx, run installcredprovider.ps1 -AddNetfx
+# To overwrite existing plugin with the latest, run installcredprovider.ps1 -Force
 # More: https://github.com/Microsoft/artifacts-credprovider/blob/master/README.md
 
 param(
-    [switch]$AddNetfx
+    [switch]$AddNetfx,
+    [switch]$Force
 )
 
 $script:ErrorActionPreference='Stop'
@@ -15,25 +17,45 @@ if ([Net.ServicePointManager]::SecurityProtocol.ToString().Split(',').Trim() -no
     [Net.ServicePointManager]::SecurityProtocol += [Net.SecurityProtocolType]::Tls12
 }
 
+$pluginLocation = "$env:USERPROFILE\.nuget\plugins"
+$tempZipLocation = "$env:TEMP\CredProviderZip"
+$localNetcoreCredProviderPath = "netcore\CredentialProvider.Microsoft"
+$localNetfxCredProviderPath = "netfx\CredentialProvider.Microsoft"
+
+# Check if plugin already exists if -Force swich is not set
+if (!$Force) {
+    $netfxExists = Test-Path -Path "$pluginLocation\$localNetfxCredProviderPath"
+    $netcoreExists = Test-Path -Path "$pluginLocation\$localNetcoreCredProviderPath"
+    if ($AddNetfx -eq $True -and $netfxExists -eq $True -and $netcoreExists -eq $True) {
+        Write-Host "The netcore and netfx Credential Providers are already in $pluginLocation"
+        return
+    }
+
+    if ($AddNetfx -eq $False -and $netcoreExists -eq $True) {
+        Write-Host "The netcore Credential Provider is already in $pluginLocation"
+        return
+    }
+}
+
 # Get the zip file from latest GitHub release
 $latestReleaseUrl = "https://api.github.com/repos/Microsoft/artifacts-credprovider/releases/latest"
 $latestRelease = Invoke-WebRequest -UseBasicParsing $latestReleaseUrl
+$zipErrorString = "Unable to resolve the Credential Provider zip file from $latestReleaseUrl"
 try {
     $latestReleaseJson = $latestRelease.Content | ConvertFrom-Json
     $zipAsset = $latestReleaseJson.assets | ? { $_.content_type -eq "application/x-zip-compressed" }
     $packageSourceUrl = $zipAsset.browser_download_url
 } catch {
-    Write-Error "Unable to resolve the Credential Provider zip file from $latestReleaseUrl"
+    Write-Error $zipErrorString
     return
 }
 
 if (!$packageSourceUrl) {
-    Write-Error "Unable to resolve the Credential Provider zip file from $latestReleaseUrl"
+    Write-Error $zipErrorString
     return
 }
 
 # Create temporary location for the zip file handling
-$tempZipLocation = "$env:TEMP\CredProviderZip"
 Write-Host "Creating temp directory for the Credential Provider zip: $tempZipLocation"
 if (Test-Path -Path $tempZipLocation) {
     Remove-Item $tempZipLocation -Force -Recurse
@@ -56,12 +78,9 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($pluginZip, $tempZipLocation)
 
 # Forcibly copy netcore (and netfx) directories to plugins directory
-$pluginLocation = "$env:USERPROFILE\.nuget\plugins"
 Write-Host "Copying Credential Provider to $pluginLocation"
-$localNetcoreCredProviderPath = "netcore\CredentialProvider.Microsoft"
 Copy-Item "$tempZipLocation\plugins\$localNetcoreCredProviderPath" -Destination "$pluginLocation\$localNetcoreCredProviderPath" -Force -Recurse
 if ($AddNetfx -eq $True) {
-    $localNetfxCredProviderPath = "netfx\CredentialProvider.Microsoft"
     Copy-Item "$tempZipLocation\plugins\$localNetfxCredProviderPath" -Destination "$pluginLocation\$localNetfxCredProviderPath" -Force -Recurse
 }
 
