1d766a301251bd482f85f0b0361dfea78e0f4922
Viktor Hofer <viktor.hofer@microsoft.com>
2019-04-03 15:51:53 +0200

    Use cross-plat dir check in installcredprovider.sh

--- helpers/installcredprovider.sh
+++ helpers/installcredprovider.sh
@@ -13,7 +13,7 @@ RELEASEURL="$GITHUB/$REPO/releases/$VERSION/download/$FILE"
 NUGET_PLUGIN_DIR="$HOME/.nuget/plugins"
 
 # Ensure plugin directory exists
-if [[ ! -e "${NUGET_PLUGIN_DIR}" ]]; then
+if [ ! -d "${NUGET_PLUGIN_DIR/}" ]; then
   echo "INFO: Creating the nuget plugin directory (i.e. ${NUGET_PLUGIN_DIR}). "
   if ! mkdir -p "${NUGET_PLUGIN_DIR}"; then
       echo "ERROR: Unable to create nuget plugins directory (i.e. ${NUGET_PLUGIN_DIR})."
