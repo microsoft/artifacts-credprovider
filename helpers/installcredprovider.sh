6e1968b6a2eb24a39e32dc811d9f5c4f4954e3c3
Satu Bailey <24945574+satbai@users.noreply.github.com>
2019-11-27 16:34:33 -0800

    Add version support to .sh script

--- helpers/installcredprovider.sh
+++ helpers/installcredprovider.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
-# DESCRIPTION: A simple shell script designed to fetch the latest version
+# DESCRIPTION: A simple shell script designed to fetch a version
 # of the artifacts credential provider plugin for dotnet and
 # install it into $HOME/.nuget/plugins.
-# SEE: https://github.com/Microsoft/artifacts-credprovider/blob/master/README.md
+# Default version to install is the latest version.
+# To install a specific version, call the script with the TAG NAME of the version,
+# e.g. "installcredprovider.sh v0.1.20". Find the tag name of the version from https://github.com/microsoft/artifacts-credprovider/releases
+# More: https://github.com/Microsoft/artifacts-credprovider/blob/master/README.md
 
 REPO="Microsoft/artifacts-credprovider"
 FILE="Microsoft.NuGet.CredentialProvider.tar.gz"
-VERSION="latest"
-# URL pattern documented at https://help.github.com/en/articles/linking-to-releases as of 2019-03-29
-URI="https://github.com/$REPO/releases/$VERSION/download/$FILE"
 NUGET_PLUGIN_DIR="$HOME/.nuget/plugins"
+
+# if no arguments, install latest version
+if [ -z "$1" ]; then
+  # URL pattern to get latest documented at https://help.github.com/en/articles/linking-to-releases as of 2019-03-29
+  URI="https://github.com/$REPO/releases/latest/download/$FILE"
+else
+  # browser_download_url from https://api.github.com/repos/Microsoft/artifacts-credprovider/releases/latest
+  VERSION=$1
+  URI="https://github.com/$REPO/releases/download/$1/$FILE"
+fi
 
 # Ensure plugin directory exists
 if [ ! -d "${NUGET_PLUGIN_DIR}" ]; then
