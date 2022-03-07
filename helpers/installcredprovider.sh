fac3d56b22d4eb7aa9862a9076d95d01d63cd6b4
Emily Bettencourt <embetten@microsoft.com>
2022-03-07 13:57:16 -0800

    Fix cred provider download url

--- helpers/installcredprovider.sh
+++ helpers/installcredprovider.sh
@@ -29,7 +29,7 @@ else
 fi
 
 # If AZURE_ARTIFACTS_CREDENTIAL_PROVIDER_VERSION is set, install the version specified, otherwise install latest
-if [[ ${AZURE_ARTIFACTS_CREDENTIAL_PROVIDER_VERSION} != "latest" ]]; then
+if [[ ! -z ${AZURE_ARTIFACTS_CREDENTIAL_PROVIDER_VERSION} ] || [ ${AZURE_ARTIFACTS_CREDENTIAL_PROVIDER_VERSION} != "latest" ]]; then
   # browser_download_url from https://api.github.com/repos/Microsoft/artifacts-credprovider/releases/latest 
   URI="https://github.com/$REPO/releases/download/${AZURE_ARTIFACTS_CREDENTIAL_PROVIDER_VERSION}/$FILE"
 else
