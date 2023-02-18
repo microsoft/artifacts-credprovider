528d3f08f38ff4c48114c9dbeacba86512bb05e5
John Schmeichel <johsch@microsoft.com>
2023-02-17 16:37:25 -0800

    Fix test script

--- test.bat
+++ test.bat
@@ -1,10 +1,11 @@
 @echo OFF
 SETLOCAL EnableDelayedExpansion
 
-@REM A Windows domain user should be able to run this against a feed in an AAD-back AzDO org 
+@REM A Windows domain user should be able to run this against a feed in an AAD-back AzDO org
 @REM and all scenarios should succeed non-interactively.
 
 set TEST_FEED=%1
+set NUGET_CREDENTIALPROVIDER_MSAL_FILECACHE_ENABLED=true
 set NUGET_CREDENTIALPROVIDER_MSAL_FILECACHE_LOCATION=%TEMP%\msal.cache
 IF EXIST %NUGET_CREDENTIALPROVIDER_MSAL_FILECACHE_LOCATION% (del %NUGET_CREDENTIALPROVIDER_MSAL_FILECACHE_LOCATION%)
 
@@ -44,12 +45,12 @@ for %%I in ("netcoreapp3.1","net461","net6.0") DO (
     del /q "!UserProfile!\AppData\Local\MicrosoftCredentialProvider\*.dat" 2>NUL
     del /q "%NUGET_CREDENTIALPROVIDER_MSAL_FILECACHE_LOCATION%" 2>NUL
     echo Testing %%I with NUGET_CREDENTIALPROVIDER_MSAL_ENABLED=!NUGET_CREDENTIALPROVIDER_MSAL_ENABLED! NUGET_CREDENTIALPROVIDER_MSAL_ALLOW_BROKER=!NUGET_CREDENTIALPROVIDER_MSAL_ALLOW_BROKER!
-    echo dotnet run --no-restore --no-build -f %%I --project CredentialProvider.Microsoft\CredentialProvider.Microsoft.csproj -- -U !TEST_FEED! -V Debug
-    dotnet run --no-restore --no-build -f %%I --project CredentialProvider.Microsoft\CredentialProvider.Microsoft.csproj -- -U !TEST_FEED! -V Debug
-    @REM  > test.%%I.%NUGET_CREDENTIALPROVIDER_MSAL_ENABLED%.%NUGET_CREDENTIALPROVIDER_MSAL_ALLOW_BROKER%.log
+    echo dotnet run --no-restore --no-build -f %%I --project CredentialProvider.Microsoft\CredentialProvider.Microsoft.csproj -- -C -U !TEST_FEED! -V Debug
+    dotnet run --no-restore --no-build -f %%I --project CredentialProvider.Microsoft\CredentialProvider.Microsoft.csproj -- -C -U !TEST_FEED! -V Debug ^
+        > test.%%I.%NUGET_CREDENTIALPROVIDER_MSAL_ENABLED%.%NUGET_CREDENTIALPROVIDER_MSAL_ALLOW_BROKER%.log
     IF !ERRORLEVEL! NEQ 0 (
         echo "Previous command execution failed: !ERRORLEVEL!"
-        dotnet run --no-restore --no-build -f %%I --project CredentialProvider.Microsoft\CredentialProvider.Microsoft.csproj -- -U !TEST_FEED! -V Debug
-        @REM exit /b !ERRORLEVEL!
+        dotnet run --no-restore --no-build -f %%I --project CredentialProvider.Microsoft\CredentialProvider.Microsoft.csproj -- -C -U !TEST_FEED! -V Debug
+        exit /b !ERRORLEVEL!
     )
 )
