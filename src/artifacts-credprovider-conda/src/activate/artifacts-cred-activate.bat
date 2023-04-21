5ac703daeffcf8b7108e23fd212abd62a2b047f2
zhenghao104 <zhentan@microsoft.com>
2023-04-21 12:08:34 -0700

    Added .bat script

--- src/artifacts-credprovider-conda/src/activate/artifacts-cred-activate.bat
+++ src/artifacts-credprovider-conda/src/activate/artifacts-cred-activate.bat
@@ -1,4 +1,4 @@
 echo Running artifacts-cred-provider-conda...
 set NUGET_CREDENTIALPROVIDER_MSAL_ENABLED=true
-for /f %%i in ('conda config --show --json ^| %CONDA_PYTHON_EXE% %~dp0%artifacts-cred.py') do "set ARTIFACTS_CONDA_TOKEN=%%i"
+@for /f %%i in ('conda config --show --json ^| conda run --name base --no-capture-output %CONDA_PYTHON_EXE% %~dp0artifacts-cred.py') do set ARTIFACTS_CONDA_TOKEN=%%i
 echo Set token to ARTIFACTS_CONDA_TOKEN environment variable.
